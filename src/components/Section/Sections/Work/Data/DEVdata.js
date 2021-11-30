64a2c27534f20644658c7ae3bd8035669762189a
Lara <larafpics@gmail.com>
2021-11-30 22:45:45 +0100

    Update work experience

--- src/components/Section/Sections/Work/Data/DEVdata.js
+++ src/components/Section/Sections/Work/Data/DEVdata.js
@@ -1,13 +1,22 @@
 export const DEVdata = [
     {
-        title: "Desarrolladora front-end - TheCookies (2021-actualidad)",
+        title: "Software Development Engineer - Plain Concepts (nov. 2021 - actualidad)",
         tasks: {
-            firstTask: "Desarrollo de páginas web con React, Gastby y Sanity",
-            secondTask: "Maquetación web con HTML5 + CSS3",
-            thirdTask: "Maquetación y desarrollo de componentes reutilizables",
-            fourthTask: "Análisis y mejoras de performance web (WPO)",
+            firstTask: "Desarrollo de aplicaciones web con React y Gatsby",
+            secondTask: "Desarrollo con headless CMS (Sanity)",
+            thirdTask: "Maquetación y desarrollo de componentes reutilizables con React",
+            fourthTask: "Desarrollo de sistemas de diseño en Storybook",
         },
     },
+    // {
+    //     title: "Desarrolladora front-end - TheCookies (2021-actualidad)",
+    //     tasks: {
+    //         firstTask: "Desarrollo de páginas web con React, Gastby y Sanity",
+    //         secondTask: "Maquetación web con HTML5 + CSS3",
+    //         thirdTask: "Maquetación y desarrollo de componentes reutilizables",
+    //         fourthTask: "Análisis y mejoras de performance web (WPO)",
+    //     },
+    // },
     // {
     //     title: "Desarrolladora web - SocialOnce (2018-2021)",
     //     tasks: {
