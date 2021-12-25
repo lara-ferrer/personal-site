707e6f98b758c2f4ae3d6a3f20c8c46183deace2
Lara <larafpics@gmail.com>
2021-12-25 16:33:19 +0100

    chore: add main image to SEO component in posts

--- src/templates/post/post.js
+++ src/templates/post/post.js
@@ -16,15 +16,13 @@ export default function post(props) {
     const { content } = post;
     
     const mapIdSection = content.map(content => content.id_section);
-
-    const image = `/${post.url}-social-card.png`;
     
     return (
         <Layout>
             <Seo 
                 title={post.Titulo}
                 description={post.Extracto}
-                image={image}
+                image={post.img_destacada.url}
             />
             
             <Container className="post">
@@ -42,18 +40,20 @@ export default function post(props) {
                         </Row>
                         <hr />
                         <Row>
-                            <Col lg={3}>
-                                <aside className="sticky">
-                                        <Scrollspy items={mapIdSection} currentClassName="is-current">
-                                        {map(content, content  => (
-                                            <li><a href={`#${content.id_section}`}>{content.heading}</a></li>
-                                            ))}
-                                        </Scrollspy>
-                                        <Button className="top button-blog" onClick={() => scrollTo('#top')}>Volver arriba</Button>
-                                </aside>
-                            </Col>
+                            {content.heading ?
+                                <Col lg={3}>
+                                    <aside className="sticky">
+                                            <Scrollspy items={mapIdSection} currentClassName="is-current">
+                                            {map(content, content  => (
+                                                <li><a href={`#${content.id_section}`}>{content.heading}</a></li>
+                                                ))}
+                                            </Scrollspy>
+                                            <Button className="top button-blog" onClick={() => scrollTo('#top')}>Volver arriba</Button>
+                                    </aside>
+                                </Col>
+                            : ''}
 
-                            <Col lg={9}>  
+                            <Col lg={content.heading ? 9 : 12}>  
                                 {map(content, content  => (
                                     <div className="markdown-body">
                                         <div id={content.id_section} className="section-post"> 
