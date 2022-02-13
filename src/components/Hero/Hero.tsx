7c9fb96bfbb556ed8d84709900b2b83de96eafef
Lara <larafpics@gmail.com>
2022-02-13 18:48:17 +0100

    chore: update classes

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -20,8 +20,8 @@ const Hero: React.FC<Hero> = () => {
 							<h1 className="hero__title">software engineer especializada en front-end y WPO</h1>
 						</div>
 					</Col>
-					<Col sm={12} md={6} lg={6} className="profile">
-						<StaticImage src="../../images/profile.png" alt="A dinosaur" placeholder="blurred" layout="constrained"/>
+					<Col sm={12} md={6} lg={6} className="hero__image">
+						<StaticImage src="../../images/profile.png" alt="Lara Ferrer" placeholder="blurred" layout="constrained"/>
 					</Col>
 				</Row>
 			</Container>
