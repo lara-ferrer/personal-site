89220f585179f0c8dc8f1280820d59f84ac7de77
Lara <larafpics@gmail.com>
2022-02-13 20:34:30 +0100

    chore: add contact button

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import Skills from "../components/Section/Sections/Skills/Skills"
 import Work from "../components/Section/Sections/Work/Work"
 import Contact from "../components/Contact/Contact"
 import Seo from "../components/Seo"
+import ContactButton from "../components/ContactButton/ContactButton"
 
 export default function Index(props) {
 	const { data } = props
@@ -16,6 +17,7 @@ export default function Index(props) {
 				title = "Inicio"
 				description = "¡Hola! Soy Lara, desarrolladora web y consultora SEO. Te animo a que visites mi página web para conocer más sobre mí :)"
 			/>
+			<ContactButton />
 			<main>
 				<Hero />
 				<About />
