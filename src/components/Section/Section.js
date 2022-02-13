332a7026ab2b4bd850b363f70a634446c750870e
Lara <larafpics@gmail.com>
2022-02-13 19:29:20 +0100

    chore: update about section

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -3,22 +3,22 @@ import { Container, Row, Col } from "react-bootstrap"
 import "./Section.scss"
 
 export class Section extends Component {
-  render() {
-    return(
-        <section>
-            <Container>
-                <Row>
-                    <Col xs={12} md={5}>
-                        <h2 className="text-gradient">{this.props.title}</h2>
-                    </Col>
-                    <Col xs={12} md={7}>
-                        {this.props.content}
-                    </Col>
-                </Row>
-            </Container>
-        </section>
-    )
-  }
+	render() {
+		return(
+			<section className="section">
+				<Container>
+					<Row>
+						<Col xs={12} md={5}>
+							<h2 className="section__title">{this.props.title}</h2>
+						</Col>
+						<Col xs={12} md={7}>
+							{this.props.content}
+						</Col>
+					</Row>
+				</Container>
+			</section>
+		)
+	}
 }
 
-export default Section;
+export default Section
