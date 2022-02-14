a44a8fc9a6449c08ba7476410f46358750e44ca3
Lara <larafpics@gmail.com>
2022-02-14 21:21:01 +0100

    chore: add animation in menu when scrolling

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Link } from "gatsby"
 import { Image } from "react-bootstrap"
 import Navbar from "react-bootstrap/Navbar"
@@ -9,8 +9,18 @@ import "./Menu.scss"
 import { AnchorLink } from "gatsby-plugin-anchor-links"
   
 export default function SecondMenu() {
+	const [scroll, setScroll] = useState(false);
+
+	useEffect(() => {
+		if (typeof window !== "undefined") {
+		  window.addEventListener("scroll", () =>
+		  	setScroll(window.pageYOffset > 300)
+		  );
+		}
+	  }, []);
+
 	return (
-		<Navbar className="menu" expand="lg" sticky="top" variant="dark" id="top">
+		<Navbar className={`menu${scroll ? ' menu--small' : ' menu--large'}`} expand="lg" sticky="top" variant="dark" id="top">
 			<Navbar.Brand href="/">
 				<Image src={logoMenu} className="logo" />
 			</Navbar.Brand>
