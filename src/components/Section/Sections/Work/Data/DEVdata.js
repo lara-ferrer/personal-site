c32fad7606c6efe48efc7d9facf84cb41378401f
Lara <larafpics@gmail.com>
2022-03-14 21:16:22 +0100

    feat: add new work experience

--- src/components/Section/Sections/Work/Data/DEVdata.js
+++ src/components/Section/Sections/Work/Data/DEVdata.js
@@ -1,24 +1,13 @@
 export const DEVdata = [
     {
-        title: "Desarrolladora front-end | TheCookies (jul 2021 - sept 2021)",
+        title: "Software Engineer | Plain Concepts (nov 2021 - actualidad)",
         tasks: {
             firstTask: "Desarrollo de aplicaciones web con React y Gatsby",
-            secondTask: "Uso de preprocesadores CSS como SASS",
-            thirdTask: "Uso de headless CMS como Sanity, empleando GraphQL para el acceso a los datos",
-            fourthTask: "Empleo de metodologías de desarrollo de software como Domain Driven Design (DDD) o arquitectura hexagonal",
+            secondTask: "Desarrollo de headless CMS con Sanity",
+            thirdTask: "Uso de preprocesadores CSS como SASS",
+            fourthTask: "Implementación y desarrollo de Design Systems con Storybook",
             fifthTask: "Empleo de metodologías de desarrollo front-end como BEM, ITCSS y Atomic Design",
             sixthTask: "Uso de TypeScript para el tipado de datos y definición de interfaces",
         },
     },
-    {
-        title: "Desarrolladora web | SocialOnce (oct 2018 - jul 2021)",
-        tasks: {
-            firstTask: "Desarrollo de páginas web responsive empleando HTML, CSS y JavaScript para el front-end, además de frameworks como Bootstrap, y PHP para el back-end",
-            secondTask: "Integración con diferentes servicios externos como Stripe para realizar pagos, Mailchimp para e-mail marketing, o Cloudinary para optimización de imágenes",
-            thirdTask: "Desarrollo de páginas web en Wordpress",
-            fourthTask: "Optimización de performance web, monitorizando las diferentes métricas disponibles y proponiendo soluciones técnicas para su mejora",
-            fifthTask: "Desarrollo y mantenimiento de tiendas on-line en WooCommerce y Prestashop",
-            sixthTask: "Diseño de interfaces en Figma",
-        },
-    }
 ];
